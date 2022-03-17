6a2804883c1112352e890b011128d81f37a6c7a6
Eric Clemmons <eric@smarterspam.com>
2022-03-17 18:57:29 -0500

    useClickToComponent()

--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -1,7 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useClickToComponent } from 'click-to-component'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useClickToComponent()
+
   return <Component {...pageProps} />
 }
 
---
+++ packages/click-to-component/src/index.ts
@@ -0,0 +1,3 @@
+export function useClickToComponent() {
+  console.log("Howdy");
+}
