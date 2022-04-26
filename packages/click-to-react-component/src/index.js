16feb7fc89a778c4b500f0a38cc1a3c8a0157cdc
Eric Clemmons <eric@smarterspam.com>
2022-04-26 16:54:39 -0500

    Fix references to files/types

--- packages/click-to-react-component/src/index.js
+++ packages/click-to-react-component/src/index.js
@@ -1,4 +1,4 @@
-import { ClickToComponent as Component } from './src/ClickToComponent.js'
+import { ClickToComponent as Component } from './ClickToComponent.js'
 
 export const ClickToComponent =
   process.env.NODE_ENV === 'development' ? Component : () => null
