d08f8f7d21390ec79a065a7e5a609a2c08733884
Alexandre BAY <alexandre.bay@skazy.nc>
2022-05-11 10:52:52 +1100

    Revert "fix(type): add intellij to export type Editor"
    This reverts commit 2522aff6f3c034dc637ad25e471cf4e6e5d869a5.

--- packages/click-to-react-component/src/types.d.ts
+++ packages/click-to-react-component/src/types.d.ts
@@ -1,6 +1,6 @@
 export { ClickToComponent } from './src/ClickToComponent'
 
-export type Editor = 'vscode' | 'vscode-insiders' | 'intellij'
+export type Editor = 'vscode' | 'vscode-insiders'
 
 export type ClickToComponentProps = {
   editor?: Editor
