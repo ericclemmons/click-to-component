13b6f4fe0024cc2c373b33e09ab927dc0cd564d8
Piyush Johnson <piyushjohnson1998@gmail.com>
2022-05-14 18:42:20 +0530

    Add example for editor from process.env

--- apps/cra/src/index.js
+++ apps/cra/src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ClickToComponent editor="vscode-insiders" />
+    <ClickToComponent editor={process.env.CTC_EDITOR} />
     <App />
   </React.StrictMode>
 );
