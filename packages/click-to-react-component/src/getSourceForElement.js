7b527f386a24a9f91b34d1b866786a9aaaac08bb
linziwei2 <linziwei2@shoplineapp.com>
2022-09-08 16:43:57 +0800

    feat: add fallback element

--- packages/click-to-react-component/src/getSourceForElement.js
+++ packages/click-to-react-component/src/getSourceForElement.js
@@ -5,6 +5,27 @@ import { getSourceForInstance } from './getSourceForInstance.js'
  * @typedef {import('react-reconciler').Fiber} Fiber
  */
 
+function getFallbackSourceForElement(
+  /**
+   * @type {HTMLElement}
+   */
+  element
+) {
+  const parentElement = element.parentElement
+  if (element.tagName === 'HTML' || parentElement === null) {
+    return
+  }
+
+  const instance = getReactInstanceForElement(parentElement)
+  const source = getSourceForInstance(instance)
+
+  if (source) {
+    return source
+  } else {
+    return getFallbackSourceForElement(parentElement)
+  }
+}
+
 export function getSourceForElement(
   /**
    * @type {HTMLElement}
@@ -17,4 +38,7 @@ export function getSourceForElement(
   if (source) return source
 
   console.warn("Couldn't find a React instance for the element", element)
+
+  const fallbackSource = getFallbackSourceForElement(element)
+  return fallbackSource
 }
