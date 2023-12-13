fc9a188bc775bf11dd1931e09bc7ee54a5e3c391
Leonidas <77194479+LeonMueller-OneAndOnly@users.noreply.github.com>
2023-12-13 18:31:37 +0100

    fix<github-issue, mui>

--- packages/click-to-react-component/src/getSourceForElement.js
+++ packages/click-to-react-component/src/getSourceForElement.js
@@ -1,20 +1,30 @@
-import { getReactInstanceForElement } from './getReactInstanceForElement.js'
-import { getSourceForInstance } from './getSourceForInstance.js'
+import { getReactInstanceForElement } from './getReactInstanceForElement'
+import { getSourceForInstance } from './getSourceForInstance'
 
 /**
  * @typedef {import('react-reconciler').Fiber} Fiber
  */
 
-export function getSourceForElement(
-  /**
-   * @type {HTMLElement}
-   */
-  element
-) {
+export function getSourceForElement(element) {
   const instance = getReactInstanceForElement(element)
   const source = getSourceForInstance(instance)
 
   if (source) return source
 
-  console.warn("Couldn't find a React instance for the element", element)
+  // console.warn("Couldn't find a React instance for the element", element)
+  // console.info("Let us try to find a React instance ancestor which has a source")
+
+  const fallbackSource = getFirstParentElementWithSource(element)
+  return fallbackSource
+}
+
+function getFirstParentElementWithSource(element) {
+  const parentElement = element.parentElement
+  if (parentElement === null) throw new Error('No parent found')
+
+  const instance = getReactInstanceForElement(parentElement)
+  const source = getSourceForInstance(instance)
+
+  if (source) return source
+  else return getFirstParentElementWithSource(element)
 }
