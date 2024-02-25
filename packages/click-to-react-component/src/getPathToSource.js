c50938bbb006e4d1b54c06f8d715f3d043f877fb
Amir <rezaamir731@gmail.com>
2024-02-25 20:39:13 +0330

    feat: -separated path params for different presets

--- packages/click-to-react-component/src/getPathToSource.js
+++ packages/click-to-react-component/src/getPathToSource.js
@@ -1,13 +1,11 @@
 /**
  * @typedef {import('react-reconciler').Source} Source
- * @typedef {import('./types').PathModifier} PathModifier
  */
 
 /**
  * @param {Source} source
- * @param {PathModifier} pathModifier
  */
-export function getPathToSource(source, pathModifier) {
+export function getPathToSource(source) {
   const {
     // It _does_ exist!
     // @ts-ignore Property 'columnNumber' does not exist on type 'Source'.ts(2339)
@@ -16,10 +14,10 @@ export function getPathToSource(source, pathModifier) {
     lineNumber = 1,
   } = source
 
-  let path = `${fileName}:${lineNumber}:${columnNumber}`
-  if (pathModifier) {
-    path = pathModifier(path)
+  return {
+    path:fileName,
+    line:lineNumber,
+    column:columnNumber
   }
 
-  return path
 }
