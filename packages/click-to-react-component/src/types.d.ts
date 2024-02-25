44e2be89d2a6146c64bc0ca065a2138afe51deb3
Amir <rezaamir731@gmail.com>
2024-02-25 20:55:00 +0330

    feat: -added getEditorUrl to props

--- packages/click-to-react-component/src/types.d.ts
+++ packages/click-to-react-component/src/types.d.ts
@@ -1,11 +1,12 @@
 export { ClickToComponent } from './ClickToComponent'
 
-export type Editor = 'vscode' | 'vscode-insiders'
+export type Editor = 'vscode' | 'vscode-insiders' | 'webstorm'
 
 export type PathModifier = (path: string) => string;
 
 export type ClickToComponentProps = {
   editor?: Editor;
+  getEditorUrl?: (params:{path:string , line:number , column:number}) => string
   pathModifier?: PathModifier;
 }
 
