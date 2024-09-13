e833ca0cae1f39a1b19b6a15d8ac3523eab23481
vision <vision@moego.pet>
2024-09-13 13:25:23 +0800

    fix: compact react lazy latest version.

--- packages/click-to-react-component/src/ContextMenu.js
+++ packages/click-to-react-component/src/ContextMenu.js
@@ -351,7 +351,9 @@ export const ContextMenu = React.forwardRef(
                         ${Object.entries(props).map(
                           ([prop, value]) => html`
                             ${' '}
-                            <var key=${prop} title="${value}">${prop}</var>
+                            <var key=${prop} title="${String(value)}"
+                              >${prop}</var
+                            >
                           `
                         )}
                         ${'>'}
